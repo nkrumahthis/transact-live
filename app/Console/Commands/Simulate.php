444b46353db0ca9580dcb884af6ab477a95b2883
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-12-25 23:28:09 +0000

    create simulate command

---
+++ app/Console/Commands/Simulate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Events\TransactionUpdated;
+use App\Models\Transaction;
+
+class Simulate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:simulate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'simulate live transactions';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $this->info("simulating transaction updates");
+        
+        while (true) {
+            // Update 10 random transactions with intervals
+            for ($i = 0; $i < 10; $i++) {
+                $transaction = Transaction::inRandomOrder()->first();
+                TransactionUpdated::dispatch($transaction);
+                usleep(100000); // Sleep for 100 milliseconds
+            }
+
+        }
+
+    }
+}
